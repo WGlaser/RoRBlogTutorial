8147a503716a84f5f325925725025c6f53f347b0
Wade Glaser <Wade.Glaser@gmail.com>
2018-01-17 14:09:48 -0500

    form-based feature tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,11 @@
 class TagsController < ApplicationController
+	def index
+		@tags = Tag.all
+	end
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		flash.notice = "Tag '#{@tag.name}' destroyed!"
+		redirect_to articles_path
+	end
 end
